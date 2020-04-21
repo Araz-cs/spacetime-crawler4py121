fcabdaed9499373ba488ea78f0ba7ea3ecfded22
Pierce Taylor <iampiercetaylor@gmail.com>
2020-04-20 17:17:41 -0700

    added extra conditions in is_valid, added code to extract_next_links to parse the unique links from resp

--- scraper.py
+++ scraper.py
@@ -1,18 +1,40 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import requests
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation requred.
-    return list()
+    if resp.status >= 400 and resp.status <= 605:
+        return list()
+
+    #Implementation requred.
+    #print("in extract_next_links")
+    soup = BeautifulSoup(resp.raw_response.content, "lxml")
+
+    seen = set()
+    
+    for link in soup.find_all('a'):
+    # get absolute urls here before adding to listLInks()
+        childURL = link.get('href')
+        if is_valid(childURL):
+            seen.add(childURL)  
+            #print(childURL)
+
+    for link in seen:
+        print (link)
+    return list(seen)
+
 
 def is_valid(url):
     try:
         parsed = urlparse(url)
-        if parsed.scheme not in set(["http", "https"]):
+        if parsed.scheme not in set(["http", "https", "today"]):
+            return False
+        if parsed.netloc not in set(["www.ics.uci.edu", "www.cs.uci.edu", "www.informatics.uci.edu", "www.stat.uci.edu", "today.uci.edu/department/information_computer_sciences"]):
             return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
